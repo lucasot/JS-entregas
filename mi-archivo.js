e5b73a1078ea293b6d88dd90c1b99b641f73c44d
LucasOtero <lucas.ot@@live.com.ar>
2021-05-02 16:48:21 -0300

    Agregando calculadora basica

---
+++ mi-archivo.js
@@ -0,0 +1,104 @@
+// REALICE UNA CALCULADORA LA CUAL EL USUARIO INGRESA SUS PROPIOS DATOS
+
+// SALUDANDO AL USUARIO
+
+function saludar() {
+    let nombre = prompt("Hola porfavor ingrese su nombre");
+    document.write("Hola "+ nombre +"</br>");
+    alert("BIENVENIDO/A " + nombre + " A MI CALCULADORA") 
+    return
+}
+saludar();
+
+var numero1;
+var numero2;
+
+function calculadora(numero1, numero2,operacion) {
+    
+
+        // DECLARANDO VARIABLES
+        var numero1 = parseInt(prompt("Ingrese el primer numero"));
+        var numero2 = parseInt(prompt("Ingrese el segundo numero"));
+        var operacion =prompt("Ingrese la operacion que desea realizar:(+,-,*,/)");
+        var r;
+        // LECTURA DE TEXTO EN PANTALLA
+        document.write("Usted ingreso como primer numero: " + numero1+"</br>");
+        document.write("Usted ingreso como segundo numero: " + numero2+"</br>");
+        document.write("Usted ingreso la operacion: "+ operacion+"</br>");
+
+        // while(operacion != "ESC") {
+
+            // SWITCH CALCULADORA
+                switch(operacion) {
+                    case "+":
+                        r = numero1 + numero2;
+                        break;
+                }
+                switch(operacion){
+                    case "-":
+                        r = numero1 - numero2;
+                        break;
+                }
+                switch(operacion){
+                    case "*":
+                        r = numero1 * numero2;
+                        break;
+                }
+                switch(operacion){
+                    case "/":
+                        r = numero1 / numero2;
+                        break;
+                default:
+                    break;
+                }
+                document.write("El resultado es " + r + "</br>");
+
+        // operacion = prompt("Ingrese otra operacion y si decia salir escriba ESC");
+}
+
+// LLAMANDO A LA FUNCION CALCULADORA
+calculadora();
+
+function mostrar(mensaje) {
+    console.log(mensaje);
+}
+
+
+/* 
+
+
+// REALICE UN PRONOSTICO DEL CLIMA MUY BASICO
+
+let entrada = prompt("Por favor ingrese un dia de la semana todo en mayusculas: ");
+while(entrada!= "ESC"){
+    switch(entrada){
+        case "LUNES":
+        alert("El dia va a estar soleado ");
+        break;
+        case "MARTES":
+        alert("El dia va a estar soleado pero con algunas nubes ");
+        break;
+        case "MIERCOLES":
+        alert("El dia va a estar nublado ");
+        break;
+        case "JUEVES":
+        alert("El dia vamos a tener lluvias ");
+        break;
+        case "VIERNES":
+        alert("El dia vamos a tener lluvias parciales ");
+        break;
+        case "SABADO":
+        alert("El dia va a estar soleado pero con algunas nubes ");
+        break;
+        case "DOMINGO":
+        alert("El dia va a estar soleado ");
+        break;
+        default:
+            alert("EL DIA INGRESADO NO EXISTE!");
+            break;
+    }
+entrada = prompt("Ingrese un dia de la semana y si decia salir escriba ESC");
+}
+
+*/ 
+
