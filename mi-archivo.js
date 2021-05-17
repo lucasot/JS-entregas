92f3b001f1034769b61ac0b15bc060c765344632
LucasOtero <lucas.ot@@live.com.ar>
2021-05-17 20:11:17 -0300

    Elimine lineas basuras

--- mi-archivo.js
+++ mi-archivo.js
@@ -1,6 +1,3 @@
-// REALICE UNA CALCULADORA LA CUAL EL USUARIO INGRESA SUS PROPIOS DATOS
-
-// SALUDANDO AL USUARIO
 function saludar() {
     let nombre = prompt("Hola porfavor ingrese su nombre");
     document.write("Hola " + nombre + "</br>");
@@ -29,7 +26,7 @@ for (n in listaNumeros) {
 */
 
 
-class Cientifica { //La class se declara siempre con la primera mayuscula.     
+class Cientifica {   
 
     constructor() {
         this.pi = 3.1416;
@@ -46,18 +43,15 @@ class Cientifica { //La class se declara siempre con la primera mayuscula.
 
     calculadora() {
 
-        // DECLARANDO VARIABLES
 
         var numero1 = parseInt(prompt("Ingrese el primer numero"));
         var numero2 = parseInt(prompt("Ingrese el segundo numero"));
         var operacion = prompt("Ingrese la operacion que desea realizar:(+,-,*,/)");
-        // LECTURA DE TEXTO EN PANTALLA
 
         document.write("Usted ingreso como primer numero: " + numero1 + "</br>");
         document.write("Usted ingreso como segundo numero: " + numero2 + "</br>");
         document.write("Usted ingreso la operacion: " + operacion + "</br>");
 
-        // SWITCH CALCULADORA
         switch (operacion) {
             case "+":
                 this.r = numero1 + numero2;
@@ -77,7 +71,7 @@ class Cientifica { //La class se declara siempre con la primera mayuscula.
         document.write("El resultado es " + this.r + "</br>");
     }
 }
-// FUNCION MOSTRAR MENSAJE 
+
 var entrada;
 
 function mostrar() {
@@ -85,134 +79,15 @@ function mostrar() {
 }
 mostrar();
 
-// LLAMANDO A FUNCION CALCULADORA
 
 var calculadora1 = new Cientifica(); // aca llamas a la class para aplicarles sus respectivos metodos.
 
-// WHILE PARA SEGUIR OPERANDO 
 while (entrada != "ESC") {
     calculadora1.calculadora();
     entrada = prompt("Para seguir realizando operaciones presione ENTER, para ver los resultados escriba ESC");
 }
 
-// FUNCIONES IVA Y PI
-
 calculadora1.sumaIva();
 document.write("El resultado con IVA es: " + calculadora1.iva + "</br>");
 calculadora1.sumaPi();
 document.write("El resultado multiplicado por pi es: " + calculadora1.pi + "</br>");
-
-
-/*
-var numero1;
-var numero2;
-  
-class cientifica{
-
-    constructor(pi ,  iva){
-
-        this.pi = pi(3,1416);
-        this.iva = iva(1.21);
-
-        function calculadora (numero1,numero2,operacion) {
-        
-            // DECLARANDO VARIABLES
-            var numero1 = parseInt(prompt("Ingrese el primer numero"));
-            var numero2 = parseInt(prompt("Ingrese el segundo numero"));
-            var operacion = prompt("Ingrese la operacion que desea realizar:(+,-,*,/)");
-            var r;
-            // LECTURA DE TEXTO EN PANTALLA
-            document.write("Usted infreso como primero numero: " + numero1 +"</br>");
-            document.write("Usted ingreso como segundo numero: " + numero2+"</br>");
-            document.write("Usted ingreso la operacion: "+ operacion+"</br>");
-
-                // SWITCH CALCULADORA
-                switch(operacion) {
-                    case "+":
-                        r = numero1 + numero2;
-                        break;
-                }
-                switch(operacion){
-                    case "-":
-                        r = numero1 - numero2;
-                        break;
-                }
-                switch(operacion){
-                    case "*":
-                        r = numero1 * numero2;
-                        break;
-                }
-                switch(operacion){
-                    case "/":
-                        r = numero1 / numero2;
-                        break;
-                default:
-                    break;
-                }
-            document.write("El resultado es " + r + "</br>");
-        }
-    }
-    sumaIva() {
-        this.iva = this.iva * r;
-    }
-    sumaPi() {
-        this.pi = this.pi * r;
-    }
-
-
-document.write("El resultado con IVA es " + sumaIva() + "</br>");
-
-document.write("El resultado multiplicado por pi es " + sumaPi() + "</br>");
-
-}
-
-// FUNCION MOSTRAR MENSAJE 
-var entrada;
-
-function mostrar() {
-    let entrada = prompt('Para usar la calculadora presione ENTER, para salir escriba ESC')
-}
-mostrar();
-
-// LLAMANDO A FUNCION CALCULADORA
-
-while (entrada != "ESC") {
-    calculadora()
-entrada = prompt("Para seguir realizando operaciones presione ENTER, para ver los resultados escriba ESC");
-}
-
-/* 
-// REALICE UN PRONOSTICO DEL CLIMA MUY BASICO
-
-let entrada = prompt("Por favor ingrese un dia de la semana todo en mayusculas: ");
-while(entrada!= "ESC"){
-    switch(entrada){
-        case "LUNES":
-        alert("El dia va a estar soleado ");
-        break;
-        case "MARTES":
-        alert("El dia va a estar soleado pero con algunas nubes ");
-        break;
-        case "MIERCOLES":
-        alert("El dia va a estar nublado ");
-        break;
-        case "JUEVES":
-        alert("El dia vamos a tener lluvias ");
-        break;
-        case "VIERNES":
-        alert("El dia vamos a tener lluvias parciales ");
-        break;
-        case "SABADO":
-        alert("El dia va a estar soleado pero con algunas nubes ");
-        break;
-        case "DOMINGO":
-        alert("El dia va a estar soleado ");
-        break;
-        default:
-            alert("EL DIA INGRESADO NO EXISTE!");
-            break;
-    }
-entrada = prompt("Ingrese un dia de la semana y si decia salir escriba ESC");
-}
-*/
-
